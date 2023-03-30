70509725cc9e60d0c30063bdc8b251aa2893312c
San'Quan Prioleau <sq.prioleau@gmail.com>
2023-03-29 22:30:16 -0400

    🗃️ Display data on page

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,25 +1,22 @@
 import { z } from "zod";
 
-import {
-  createTRPCRouter,
-  publicProcedure,
-  protectedProcedure,
-} from "~/server/api/trpc";
+import { createTRPCRouter, publicProcedure, protectedProcedure } from "~/server/api/trpc";
 
 export const exampleRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
+	hello: publicProcedure.input(z.object({ text: z.string() })).query(async ({ input, ctx }) => {
+		const data = await ctx.prisma.example.findMany();
 
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
+		return {
+			greeting: `Hello ${input.text}`,
+			data,
+		};
+	}),
 
-  getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!";
-  }),
+	getAll: publicProcedure.query(({ ctx }) => {
+		return ctx.prisma.example.findMany();
+	}),
+
+	getSecretMessage: protectedProcedure.query(() => {
+		return "you can now see this secret message!";
+	}),
 });
