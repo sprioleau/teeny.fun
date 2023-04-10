fe0c9d1eaa538b5d16837f4c0db2bcfed03c17a4
San'Quan Prioleau <sq.prioleau@gmail.com>
2023-04-10 06:18:11 -0400

    ⚡️ Use optimized fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,34 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { Inter, Modak } from "next/font/google";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
+const inter = Inter({
+	subsets: ["latin"],
+	variable: "--ff-sans",
+});
+
+const modak = Modak({
+	variable: "--ff-serif",
+	subsets: ["latin"],
+	weight: "400",
+});
+
 const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
+	Component,
+	pageProps: { session, ...pageProps },
 }) => {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+	return (
+		<SessionProvider session={session}>
+			<div className={inter.className}>
+				<Component {...pageProps} />
+			</div>
+		</SessionProvider>
+	);
 };
 
 export default api.withTRPC(MyApp);
