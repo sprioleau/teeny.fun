a6578247a424ebd07e1a2cd4dcf95817b67fe725
San'Quan Prioleau <sq.prioleau@gmail.com>
2023-04-10 06:17:51 -0400

    🎨 Add prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,40 +3,39 @@ const path = require("path");
 
 /** @type {import("eslint").Linter.Config} */
 const config = {
-  overrides: [
-    {
-      extends: [
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-      ],
-      files: ["*.ts", "*.tsx"],
-      parserOptions: {
-        project: path.join(__dirname, "tsconfig.json"),
-      },
-    },
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: path.join(__dirname, "tsconfig.json"),
-  },
-  plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
-  rules: {
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
-    ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    "@typescript-eslint/restrict-template-expressions": [
-      "warn",
-      {
-        allowNullish: false,
-        allowString: true,
-      },
-    ],
-  },
+	overrides: [
+		{
+			extends: ["plugin:@typescript-eslint/recommended-requiring-type-checking"],
+			files: ["*.ts", "*.tsx"],
+			parserOptions: {
+				project: path.join(__dirname, "tsconfig.json"),
+			},
+		},
+	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		project: path.join(__dirname, "tsconfig.json"),
+	},
+	plugins: ["@typescript-eslint", "prettier"],
+	extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended", "prettier"],
+	rules: {
+		"prettier/prettier": "error",
+		"@typescript-eslint/consistent-type-imports": [
+			"warn",
+			{
+				prefer: "type-imports",
+				fixStyle: "inline-type-imports",
+			},
+		],
+		"@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+		"@typescript-eslint/restrict-template-expressions": [
+			"warn",
+			{
+				allowNullish: false,
+				allowString: true,
+			},
+		],
+	},
 };
 
 module.exports = config;
