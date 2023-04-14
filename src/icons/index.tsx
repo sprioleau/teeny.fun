5ce758682f9bff7e2533388ae518ed73badc2e6f
San'Quan Prioleau <sq.prioleau@gmail.com>
2023-04-14 11:02:36 -0400

    🐛 Fix prop names

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -10,9 +10,9 @@ export function SubmitIcon() {
 			<path
 				d="M8.44444 7.84521L4 12.8452M4 12.8452L8.44444 17.8452M4 12.8452H20V8.84521"
 				stroke="#24284D"
-				stroke-width="3"
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeWidth="3"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 			/>
 		</svg>
 	);
