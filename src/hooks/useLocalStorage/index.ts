0ddad285ec48b6e04c5e1f1d440c796bb9e96f5f
San'Quan Prioleau <sq.prioleau@gmail.com>
2023-04-24 23:21:08 -0400

    👔 Add hok for accessing localStorage

---
+++ src/hooks/useLocalStorage/index.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+export default function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T) => void] {
+	const [storedValue, setStoredValue] = useState<T>(initialValue);
+
+	useEffect(() => {
+		// Retrieve from localStorage
+		const item = localStorage.getItem(key);
+		if (item) setStoredValue(JSON.parse(item) as T);
+	}, [key]);
+
+	const setValue = (value: T) => {
+		setStoredValue(value);
+		// Persist to localStorage
+		localStorage.setItem(key, JSON.stringify(value));
+	};
+
+	return [storedValue, setValue];
+}
