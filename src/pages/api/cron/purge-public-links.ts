4bf20c38c02331ea1eb3077358c51a4d2e523388
San'Quan Prioleau <sq.prioleau@gmail.com>
2023-04-26 21:54:17 -0400

    👔 Add serverless function to handle public link deletion

---
+++ src/pages/api/cron/purge-public-links.ts
@@ -0,0 +1,39 @@
+import { type NextApiRequest, type NextApiResponse } from "next";
+import { PROJECT_REPO_URL } from "~/constants/projectRepoUrl";
+import { prisma } from "~/server/db";
+
+/*
+ * See verce.json for schedule:
+ * Runs at 1:00PM UTC (9:00AM EST)
+ */
+
+const MINUTES_UNTIL_RESET = 60 * 24; // 1 day
+const MILLISECONDS_PER_MINUTE = 1000 * 60; // Represents 1 minute in milliseconds
+
+export default async function handler(request: NextApiRequest, response: NextApiResponse) {
+	const millisecondsToExpire = MILLISECONDS_PER_MINUTE * MINUTES_UNTIL_RESET;
+	const dateExpired = new Date(Date.now() - millisecondsToExpire).toISOString();
+
+	const urlsToDelete = await prisma.url.deleteMany({
+		where: {
+			AND: [
+				{ user: null },
+				{
+					createdAt: {
+						lte: dateExpired,
+					},
+				},
+			],
+			NOT: [
+				{
+					destinationUrl: PROJECT_REPO_URL,
+				},
+			],
+		},
+	});
+
+	response.status(200).json({
+		ok: true,
+		urlsToDelete,
+	});
+}
