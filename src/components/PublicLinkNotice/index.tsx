a7d0a6784e157a761d83e05b8052297b8e53a995
San'Quan Prioleau <sq.prioleau@gmail.com>
2023-05-01 06:39:10 -0400

    ✏️ Correct public link duration

--- src/components/PublicLinkNotice/index.tsx
+++ src/components/PublicLinkNotice/index.tsx
@@ -5,7 +5,7 @@ export default function PublicLinkNotice() {
 	return (
 		<div className={styles["main"]}>
 			<p>
-				Note: Public links will be automatically-deleted after 30 minutes.{" "}
+				Note: Public links will be automatically-deleted after 24 hours.{" "}
 				<Link href="/auth/signin">Create a free account</Link> to keep your links forever.
 			</p>
 		</div>
