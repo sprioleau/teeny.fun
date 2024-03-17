853129614fba59b8e3f498ebf43d6dc43ae4f0df
San'Quan Prioleau <sq.prioleau@gmail.com>
2024-03-17 07:29:35 -0400

    ✨ Add util for parsing formData by schema

---
+++ src/utils/getParsedFormData.ts
@@ -0,0 +1,18 @@
+import { ZodType } from "zod";
+
+export default function getParsedFormData<Schema extends ZodType>({
+	formData,
+	schema,
+}: {
+	formData: FormData;
+	schema: Schema;
+}): Schema extends ZodType<infer T> ? T : never {
+	const formDataObject = Object.fromEntries(formData.entries());
+	const parsedFormData = schema.safeParse(formDataObject);
+
+	if (!parsedFormData.success) {
+		throw new Error("Invalid form data");
+	}
+
+	return parsedFormData.data;
+}
