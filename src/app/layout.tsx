f3c2660344c461db21ec3267b62a2e2f6ec60867
San'Quan Prioleau <sq.prioleau@gmail.com>
2024-03-17 07:24:14 -0400

    ♻️ Restore ModalContextProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,11 @@
+import { Footer, Navigation } from "@/components";
+import { ModalContextProvider } from "@/contexts";
+import Page from "@/layout/Page";
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Modak, Space_Grotesk } from "next/font/google";
-
-import { Navigation, Footer } from "@/components";
-import { ModalContextProvider } from "@/contexts";
-import { Toaster } from "react-hot-toast";
-import Page from "@/layout/Page";
 import { Suspense } from "react";
+import { Toaster } from "react-hot-toast";
 
 import "@/styles/globals.scss";
 
@@ -36,26 +35,24 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 		<ClerkProvider>
 			<html lang="en">
 				<body>
-					{/* <ModalContextProvider> */}
-					<div className={["app", modak.variable, spaceGrotesk.variable].join(" ")}>
-						<Navigation />
-						<Suspense fallback={<p>Loading...</p>}>
+					<ModalContextProvider>
+						<div className={["app", modak.variable, spaceGrotesk.variable].join(" ")}>
+							<Navigation />
 							<Page>{children}</Page>
-						</Suspense>
-						<Footer />
-						<Toaster
-							containerStyle={{ top: "calc(1.5em + var(--page-padding-y))" }}
-							toastOptions={{
-								className: "toaster",
-								duration: 3500,
-								ariaProps: {
-									role: "status",
-									"aria-live": "polite",
-								},
-							}}
-						/>
-					</div>
-					{/* </ModalContextProvider> */}
+							<Footer />
+							<Toaster
+								containerStyle={{ top: "calc(1.5em + var(--page-padding-y))" }}
+								toastOptions={{
+									className: "toaster",
+									duration: 3500,
+									ariaProps: {
+										role: "status",
+										"aria-live": "polite",
+									},
+								}}
+							/>
+						</div>
+					</ModalContextProvider>
 				</body>
 			</html>
 		</ClerkProvider>
