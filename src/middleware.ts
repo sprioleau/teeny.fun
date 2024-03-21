8bd548990ad54620c9cc5ccaf08f1651d96adc17
San'Quan Prioleau <sq.prioleau@gmail.com>
2024-03-21 09:21:41 -0400

    ♻️ Ensure all static files are ignored

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,8 +8,8 @@ export default clerkMiddleware((auth, req) => {
 export const config = {
 	// prettier-ignore
 	matcher: [
-		"/((?!.*\\..*|_next).*)", // Don't run middleware on static files
-		"/"                     , // Run middleware on index page
-		// "/(api|trpc)(.*)"       , // Run middleware on API routes
+		"/((?!.*\\..*|_next|_static|_next|_vercel).*)", // Don't run middleware on static files
+		"/" , 																					// Run middleware on index page
+		// "/(api|trpc)(.*)", 													// Run middleware on API routes
 	],
 };
