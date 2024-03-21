ebe63787f1d532c4985ff3b6d94721e3e05bd589
San'Quan Prioleau <sq.prioleau@gmail.com>
2024-03-21 07:19:21 -0400

    ♻️ Add publishable key in provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export const metadata: Metadata = homepageMetadata;
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<ClerkProvider>
+		<ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
 			<html lang="en">
 				<body>
 					<ModalContextProvider>
