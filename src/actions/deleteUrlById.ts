ad59bac119394b9d742e904f907ac2cf99f92167
San'Quan Prioleau <sq.prioleau@gmail.com>
2024-04-14 22:16:03 -0400

    ✨ Allow edit and delete actions for unauthenticated users

--- src/actions/deleteUrlById.ts
+++ src/actions/deleteUrlById.ts
@@ -1,11 +1,13 @@
 "use server";
 
+import { PERSISTED_CLIENT_KEY } from "@/constants";
 import { db } from "@/db";
 import { urls } from "@/db/schema";
 import { getParsedFormData } from "@/utils";
-import { auth } from "@clerk/nextjs/server";
+// import { auth } from "@clerk/nextjs/server";
 import { and, eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
+import { cookies } from "next/headers";
 import { z } from "zod";
 
 const formDataSchema = z.object({
@@ -14,22 +16,30 @@ const formDataSchema = z.object({
 
 export default async function deleteUrlById(formData: FormData) {
 	try {
-		const { userId: authenticatedUserId } = auth();
+		const clientKey = cookies().get(PERSISTED_CLIENT_KEY)?.value;
 
-		if (!authenticatedUserId) {
-			throw new Error("Not authenticated");
+		if (!clientKey) {
+			throw new Error("Invalid client key");
 		}
+
+		// const { userId: authenticatedUserId } = auth();
+
+		// if (!authenticatedUserId) {
+		// 	throw new Error("Not authenticated");
+		// }
 
 		const parsedFormData = getParsedFormData({
 			formData,
 			schema: formDataSchema,
 		});
 
-		await db
-			.delete(urls)
-			.where(
-				and(eq(urls.id, parsedFormData["url-id"]), eq(urls.userAuthProviderId, authenticatedUserId))
-			);
+		await db.delete(urls).where(
+			and(
+				eq(urls.id, parsedFormData["url-id"]),
+				eq(urls.clientKey, clientKey)
+				// eq(urls.userAuthProviderId, authenticatedUserId),
+			)
+		);
 
 		revalidatePath("/");
 	} catch (caughtError) {
--- src/actions/updateShortcodeById.ts
+++ src/actions/updateShortcodeById.ts
@@ -1,29 +1,34 @@
 "use server";
 
+import { PERSISTED_CLIENT_KEY } from "@/constants";
 import { db } from "@/db";
 import { urls } from "@/db/schema";
 import { emojiToCodePoints, getParsedFormData } from "@/utils";
-import { auth } from "@clerk/nextjs/server";
+// import { auth } from "@clerk/nextjs/server";
 import { and, eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
+import { cookies } from "next/headers";
 import { z } from "zod";
+import { codeSchema } from "./schemas";
 
 const formDataSchema = z.object({
 	"url-id": z.coerce.number(),
-	code: z
-		.string()
-		.emoji("Only emojis are allowed")
-		.min(3, "Must be at least 3 emojis")
-		.max(6, "Must be at most 6 emojis"),
+	code: codeSchema,
 });
 
 export default async function updateShortcodeById(formData: FormData) {
 	try {
-		const { userId: authenticatedUserId } = auth();
+		const clientKey = cookies().get(PERSISTED_CLIENT_KEY)?.value;
 
-		if (!authenticatedUserId) {
-			throw new Error("Not authenticated");
+		if (!clientKey) {
+			throw new Error("Invalid client key");
 		}
+
+		// const { userId: authenticatedUserId } = auth();
+
+		// if (!authenticatedUserId) {
+		// 	throw new Error("Not authenticated");
+		// }
 
 		const parsedFormData = getParsedFormData({
 			formData,
@@ -37,7 +42,11 @@ export default async function updateShortcodeById(formData: FormData) {
 				codePoints: emojiToCodePoints(parsedFormData["code"]),
 			})
 			.where(
-				and(eq(urls.id, parsedFormData["url-id"]), eq(urls.userAuthProviderId, authenticatedUserId))
+				and(
+					eq(urls.id, parsedFormData["url-id"]),
+					eq(urls.clientKey, clientKey)
+					// eq(urls.userAuthProviderId, authenticatedUserId)
+				)
 			);
 
 		revalidatePath("/");
