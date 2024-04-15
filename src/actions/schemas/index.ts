45d228e5a2f4577ac22aefd5a9eabf624536f15d
San'Quan Prioleau <sq.prioleau@gmail.com>
2024-04-14 22:15:29 -0400

    🐛 Fix emoji schema parsing so that emoji length count is acurate

--- src/actions/schemas/index.ts
+++ src/actions/schemas/index.ts
@@ -1,10 +1,39 @@
+import { ERROR_MESSAGES } from "@/constants";
 import { z } from "zod";
+import GraphemeSplitter from "grapheme-splitter";
+
+const splitter = new GraphemeSplitter();
+
+function normalizeUrlLikeString(string: string) {
+	const SECERE_PROTOCOL = "https://";
+	return string.startsWith(SECERE_PROTOCOL) ? string : `${SECERE_PROTOCOL}${string}`;
+}
+
+export const codeSchema = z
+	.string()
+	.emoji("Only emojis are allowed")
+	.superRefine((emojiString, context) => {
+		const emojisCount = splitter.countGraphemes(emojiString);
+
+		if (emojisCount < 3) {
+			context.addIssue({
+				code: z.ZodIssueCode.custom,
+				message: "Must be at least 3 emojis",
+			});
+		}
+
+		if (emojisCount > 6) {
+			context.addIssue({
+				code: z.ZodIssueCode.custom,
+				message: "Must be at most 6 emojis",
+			});
+		}
+	});
 
 export const createUrlSchema = z.object({
-	"destination-url": z.string().url("Invalid URL"),
-	code: z
+	"destination-url": z
 		.string()
-		.emoji("Only emojis are allowed")
-		.min(3, "Must be at least 3 emojis")
-		.max(6, "Must be at most 6 emojis"),
+		.transform(normalizeUrlLikeString)
+		.pipe(z.string().url(ERROR_MESSAGES.INVALID_URL)),
+	code: codeSchema,
 });
