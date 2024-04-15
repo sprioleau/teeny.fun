fb8fedc23b2462ca745c9a24a369340b939c2f88
San'Quan Prioleau <sq.prioleau@gmail.com>
2024-04-14 22:18:50 -0400

    👔 Add error messages to constants

---
+++ src/constants/errorMessages.ts
@@ -0,0 +1,3 @@
+export const ERROR_MESSAGES = {
+	INVALID_URL: "That URL is invalid.",
+};
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
-export { TOP_EMOJIS } from "./topEmojis";
+export { ERROR_MESSAGES } from "./errorMessages";
 export * from "./localStorageKeys";
 export { PROJECT_REPO } from "./projectRepoUrl";
+export { TOP_EMOJIS } from "./topEmojis";
