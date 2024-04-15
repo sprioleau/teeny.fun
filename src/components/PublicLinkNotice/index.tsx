af656c11db725aa068d0e5826599cfccac6996bb
San'Quan Prioleau <sq.prioleau@gmail.com>
2024-04-14 22:17:37 -0400

    💬 Update `PublicLinkNotice` component to indicate that all links will be deleted after 24 hours

--- src/components/PublicLinkNotice/index.tsx
+++ src/components/PublicLinkNotice/index.tsx
@@ -1,6 +1,10 @@
-import { ClerkLoaded, SignUpButton as ClerkSignUpButton, SignedOut } from "@clerk/nextjs";
-import { HiOutlineRocketLaunch } from "react-icons/hi2";
-import Button from "../Button";
+import {
+	ClerkLoaded,
+	// SignUpButton as ClerkSignUpButton,
+	SignedOut,
+} from "@clerk/nextjs";
+// import { HiOutlineRocketLaunch } from "react-icons/hi2";
+// import Button from "../Button";
 
 import styles from "./index.module.scss";
 
@@ -11,21 +15,21 @@ export default function PublicLinkNotice() {
 		<ClerkLoaded>
 			<SignedOut>
 				<div className={styles["main"]}>
-					<p>Public links will be automatically-deleted after 24 hours.</p>
+					<p>All links will be automatically-deleted after 24 hours.</p>
 					{/* {isLimitExceeded && (
 						<p>
 							Maximum number of links reached. Either delete existing links or create a free
 							account.
 						</p>
 					)} */}
-					<ClerkSignUpButton mode="modal">
+					{/* <ClerkSignUpButton mode="modal">
 						<Button
 							icon={<HiOutlineRocketLaunch />}
 							color="blue"
 						>
 							Create a free account
 						</Button>
-					</ClerkSignUpButton>
+					</ClerkSignUpButton> */}
 				</div>
 			</SignedOut>
 		</ClerkLoaded>
